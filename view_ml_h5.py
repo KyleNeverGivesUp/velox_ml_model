d827dd33f8831d90e54efc6612e1864e04d49115
KyleAtASU <shunjieh@asu.edu>
2025-04-14 15:49:55 -0700

    initial

---
+++ view_ml_h5.py
@@ -0,0 +1,13 @@
+import h5py
+
+if __name__ == '__main__':
+
+    h5_file_path = "job_any_64.h5"
+
+    with h5py.File(h5_file_path, "r") as f:
+        print("model paras list:")
+        for key in f.keys():
+            data = f[key][()]
+            print(f"{key}: shape = {data.shape}")
+            print(data[:5])  # print first 5 rows
+            print("-" * 40)
