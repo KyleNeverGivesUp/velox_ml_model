d71ea2ccec685914aba29c5f2f08d53de3295ded
KyleAtASU <shunjieh@asu.edu>
2025-04-14 18:48:15 -0700

    new code

---
+++ gen_nn_model.py
@@ -0,0 +1,28 @@
+import torch
+import torch.nn as nn
+import h5py
+
+if "__main__" == __name__:
+
+    # define model
+    class Net(nn.Module):
+        def __init__(self):
+            super(Net, self).__init__()
+            self.fc1 = nn.Linear(100, 64)
+            self.fc2 = nn.Linear(64, 16)
+            self.fc3 = nn.Linear(16, 2)
+
+        def forward(self, x):
+            x = torch.relu(self.fc1(x))
+            x = torch.relu(self.fc2(x))
+            x = self.fc3(x)
+            return x
+
+    model = Net()
+
+    # export .h5 file
+    with h5py.File("nn-test-model.h5", "w") as f:
+        for name, param in model.named_parameters():
+            f.create_dataset(name, data=param.detach().numpy())
+
+    print("job_any_64.h5 saved successfully.")
