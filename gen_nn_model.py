9e9d81f4019508943999c2523374323d75d0321b
KyleAtASU <shunjieh@asu.edu>
2025-04-14 21:16:17 -0700

    update

--- gen_nn_model.py
+++ gen_nn_model.py
@@ -11,6 +11,7 @@ if "__main__" == __name__:
             self.fc1 = nn.Linear(100, 64)
             self.fc2 = nn.Linear(64, 16)
             self.fc3 = nn.Linear(16, 2)
+            self.fc4 = nn.Linear(16, 1)
 
         def forward(self, x):
             x = torch.relu(self.fc1(x))
@@ -23,6 +24,7 @@ if "__main__" == __name__:
     # export .h5 file
     with h5py.File("nn-test-model.h5", "w") as f:
         for name, param in model.named_parameters():
+            print(name, param)
             f.create_dataset(name, data=param.detach().numpy())
 
     print("job_any_64.h5 saved successfully.")
