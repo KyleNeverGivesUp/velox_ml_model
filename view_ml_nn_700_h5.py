67e7afc9a6a2198ee0e1e3adca4566b94914f55a
KyleAtASU <shunjieh@asu.edu>
2025-04-16 03:18:42 -0700

    nn 700 h5

---
+++ view_ml_nn_700_h5.py
@@ -0,0 +1,14 @@
+import h5py
+
+if __name__ == '__main__':
+
+    h5_file_path = "job_any_128_700.h5"
+
+    with h5py.File(h5_file_path, "r") as f:
+        print("model paras list:")
+        for key in f.keys():
+            data = f[key][()]
+            print(f"{key}: shape = {data.shape}")
+            print(data[:1])  # print first 5 rows
+            # print(data[:5])  # print first 5 rows
+            print("-" * 40)
